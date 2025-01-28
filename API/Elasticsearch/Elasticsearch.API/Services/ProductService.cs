7e65304233b9eeeaa8da616046f45b222b47c93e
ensar.akkuzey <ensarakkuzey@proton.me>
2025-01-28 17:06:48 +0300

    Create services and new dto structure

---
+++ API/Elasticsearch/Elasticsearch.API/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using Elasticsearch.API.DTOs;
+using Elasticsearch.API.Repositories;
+using System.Net;
+
+namespace Elasticsearch.API.Services
+{
+    public class ProductService
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductService(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<ResponseDto<ProductDto>> SaveAsync(ProductCreateDto request)
+        {
+            var response = await _productRepository.SaveAsync(request.CreateProduct());
+
+            if (response == null)
+            {
+                return ResponseDto<ProductDto>.Fail(new List<string> {"An error occurred while saving the product." }, HttpStatusCode.InternalServerError);
+            }
+
+            return ResponseDto<ProductDto>.Success(response.CreateDto(), HttpStatusCode.Created);
+
+        }
+    }
+}
