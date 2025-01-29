ac264123e876e9a961a993f90b337fed875d9677
ensar.akkuzey <ensarakkuzey@proton.me>
2025-01-30 01:00:58 +0300

    Add Repository and Service for ECommerce example.

---
+++ API/Elasticsearch/Elasticsearch.WEB/Repositories/ECommerceRepository.cs
@@ -0,0 +1,62 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
+using Elasticsearch.WEB.Models;
+using Elasticsearch.WEB.ViewModels;
+
+namespace Elasticsearch.WEB.Repositories
+{
+    public class ECommerceRepository
+    {
+        private readonly ElasticsearchClient _client;
+        private const string indexName = "kibana_sample_data_ecommerce";
+
+        public ECommerceRepository(ElasticsearchClient client)
+        {
+            _client = client;
+        }
+
+
+        public async Task<(List<ECommerce>, long Count)> SearchAsync(ECommerceSearchViewModel searchViewModel, int page, int pageSize)
+        {
+            List<Action<QueryDescriptor<ECommerce>>> listQuery = new();
+
+            if (!string.IsNullOrEmpty(searchViewModel.Category))
+            {
+                Action<QueryDescriptor<ECommerce>> query = q => q.Match(m => m.Field(f => f.Category).Query(searchViewModel.Category));
+                listQuery.Add(query);
+            }
+
+            if (!string.IsNullOrEmpty(searchViewModel.CustomerFullName))
+            {
+                Action<QueryDescriptor<ECommerce>> query = q => q.Match(m => m.Field(f => f.CustomerFullName).Query(searchViewModel.CustomerFullName));
+                listQuery.Add(query);
+            }
+
+            if (searchViewModel.OrderDateStart.HasValue)
+            {
+                Action<QueryDescriptor<ECommerce>> query = q => q.Range(r => r.DateRange(d => d.Field(f => f.OrderDate).Gte(searchViewModel.OrderDateStart.Value)));
+                listQuery.Add(query);
+            }
+
+            if (searchViewModel.OrderDateEnd.HasValue)
+            {
+                Action<QueryDescriptor<ECommerce>> query = q => q.Range(r => r.DateRange(d => d.Field(f => f.OrderDate).Lte(searchViewModel.OrderDateEnd.Value)));
+                listQuery.Add(query);
+            }
+
+            if (!string.IsNullOrEmpty(searchViewModel.Gender))
+            {
+                Action<QueryDescriptor<ECommerce>> query = q => q.Term(t => t.Field(f => f.CustomerFullName).Value(searchViewModel.Gender));
+            }
+
+            var pageFrom = (page - 1) * pageSize;
+
+            var result = await _client.SearchAsync<ECommerce>(x => x.Index(indexName).Query(q => q.Bool(b => b.Must(listQuery.ToArray()))).From(pageFrom).Size(pageSize));
+
+            foreach (var hit in result.Hits) hit.Source.Id = hit.Id;
+
+            return (result.Documents.ToList(), result.Total);
+        }
+
+    }
+}
---
+++ API/Elasticsearch/Elasticsearch.WEB/Services/ECommerceService.cs
@@ -0,0 +1,36 @@
+﻿using Elasticsearch.WEB.Repositories;
+using Elasticsearch.WEB.ViewModels;
+
+namespace Elasticsearch.WEB.Services
+{
+    public class ECommerceService
+    {
+        private readonly ECommerceRepository _eCommerceRepository;
+
+        public ECommerceService(ECommerceRepository eCommerceRepository)
+        {
+            _eCommerceRepository = eCommerceRepository;
+        }
+
+        public async Task<(List<ECommerceViewModel>, long Count, int pageCount)> SearchAsync(ECommerceSearchViewModel searchViewModel, int page, int pageSize)
+        {
+            var (list, totalCount) = await _eCommerceRepository.SearchAsync(searchViewModel, page, pageSize);
+            var pageLinkCount = (int)Math.Ceiling((double)totalCount / pageSize);
+
+            var eCommerceList = list.Select(x => new ECommerceViewModel
+            {
+                Id = x.Id,
+                CustomerFirstName = x.CustomerFirstName,
+                CustomerLastName = x.CustomerLastName,
+                CustomerFullName = x.CustomerFullName,
+                Gender = x.Gender,
+                OrderId = x.OrderId,
+                OrderDate = x.OrderDate.ToString("yyyy-MM-dd"),
+                TaxfulTotalPrice = x.TaxfulTotalPrice,
+                Category = String.Join(",", x.Category)
+            });
+
+            return (eCommerceList.ToList(), totalCount, pageLinkCount);
+        }
+    }
+}
